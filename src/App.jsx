1e05ef98877ba4d81d3e765824c74c6b4b0de978
senyagame <arseniyantropov3@gmail.com>
2024-11-01 16:04:16 +0500

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Regions from './Regions'; // Пример страницы
+import Regions from './Regions';
 import Page1 from './Page1';
 import Page2 from './Page2';
 import Page3 from './Page3';
